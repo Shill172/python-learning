f76d0db7e985a2bafb9b1ded109c4930c8794df1
Scott <scotthill006@gmail.com>
2025-05-11 16:00:55 +0100

    Data Types

---
+++ Basic_Programs/DataTypes.py
@@ -0,0 +1,64 @@
+# Data types - I will come back and add to this file if I encounter a new data type
+
+# String
+strVar = "Hello, World!"
+print("strVar:", strVar)
+print(type(strVar), "\n")
+
+
+# int
+intVar = 12
+print("intVar:", intVar)
+print(type(intVar), "\n")
+
+
+# float
+floatVar = 3.14
+print("floatVar:", floatVar)
+print(type(floatVar), "\n")
+
+
+# complex
+complexVar = 2j
+print("complexVar:", complexVar)
+print(type(complexVar), "\n")
+
+
+# boolean
+booleanVar = True
+print("booleanVar:", booleanVar)
+print(type(booleanVar), "\n")
+
+
+# list
+listVar = [1, 2, 3]
+print("listVar:", listVar)
+print(type(listVar), "\n")
+
+
+# tuple (immutable list like using final in Java)
+tupleVar = (1, 2, 3)
+print("tupleVar:", tupleVar)
+print(type(tupleVar), "\n")
+
+
+# set (unordered collection like HashSet in Java)
+setVar = {1, 2, 3}
+print("setVar:", setVar)
+print(type(setVar), "\n")
+
+
+# dictionary (key/values like HashMap in Java)
+dictVar = {"Name": "Hulkenberg", "Hungry?": "Always"}
+print("dictVar:", dictVar)
+print(type(dictVar), "\n")
+
+# nothing
+noneVar = None
+print("noneVar:", noneVar)
+print(type(noneVar))
+
+
+
+
+
