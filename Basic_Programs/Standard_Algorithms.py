02edac855fdfb9d6162e4aa39c4ed026dad66ff7
Scott <scotthill006@gmail.com>
2025-05-14 14:14:43 +0100

    added more standard algorithms

--- Basic_Programs/Standard_Algorithms.py
+++ Basic_Programs/Standard_Algorithms.py
@@ -36,7 +36,7 @@ print(min(levels),"\n")
 
 #Searching in a list
 characters = ["Monotaro", "Monodam", "Monokid", "Monophanie", "Monosuke"]
-choice = str(input("Search for a Mono-cub!"))
+choice = str(input("Search for a Mono-cub! "))
 for i in characters:
     if choice == i:
         print("Found!")
@@ -48,22 +48,60 @@ else:
 #Count item in a list
 numbers = [1, 3, 4, 5, 2, 6, 7, 3, 2, 1, 1, 5, 5, 6, 8, 2, 9, 9, 9, 9, 9, 9]
 count = 0
-Countchoice = int(input("Select a number between 1 and 9"))
+Countchoice = int(input("Select a number between 1 and 9: "))
 for i in numbers:
     if Countchoice == i:
         count = count + 1
 print("There are ", count, " ", Countchoice, "'s")
 
+print()
+
 
 # Sum values
+sum_ = 0
+for i in numbers:
+    sum_ = i + sum_
+print("The sum of numbers array is: ", sum_)
+
+## Can also do this for sum
+print(sum(numbers))
 
 
 # Reverse list
+reverse_this = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+temp1 = -1
+                        # Iterate through half of list
+for i, num in enumerate(reverse_this[:len(reverse_this)//2]):
+        #Position at: Length of list - index - 1 because of zero index    
+    temp1 = reverse_this[len(reverse_this) - i - 1]   #Store current num going to be replaced
+    reverse_this[len(reverse_this) - i - 1] = reverse_this[i]
+    reverse_this[i] = temp1
+print(reverse_this)
 
 
 # Sorting lists
+## Bubble sort
+temp_bubble = -1 
+bubble_sort = [4, 5, 2, 1, 7, 3, 6]
+for i in enumerate(bubble_sort):
+    for j, value in enumerate(bubble_sort[:len(bubble_sort)-1]):
+        if bubble_sort[j] > bubble_sort[j+1]:
+            temp_bubble = bubble_sort[j+1]
+            bubble_sort[j+1] = bubble_sort[j]
+            bubble_sort[j] = temp_bubble
+print(bubble_sort)
 
+## Another method of bubble sort
+bubble_sort2 = [4, 5, 2, 1, 7, 3, 6] 
+n = len(bubble_sort2)
 
+for i in range(n):
+    for j in range(n - 1 - i):
+        if bubble_sort2[j] > bubble_sort2[j+1]:
+             bubble_sort2[j], bubble_sort2[j+1] = bubble_sort2[j+1], bubble_sort2[j]
+print(bubble_sort2)
 
-    
- 
+## Simpler way
+sort3 = [4, 5, 2, 1, 7, 3, 6] 
+sort3.sort()
+print(sort3)
