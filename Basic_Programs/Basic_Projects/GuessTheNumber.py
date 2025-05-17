0b49a2008e201e667559607de6cdc20346722610
Scott <scotthill006@gmail.com>
2025-05-17 12:27:06 +0100

    ammended code to not break when not an int is entered using exception

--- Basic_Programs/Basic_Projects/GuessTheNumber.py
+++ Basic_Programs/Basic_Projects/GuessTheNumber.py
@@ -3,23 +3,26 @@ import random
 # Basic guess the number game to practice modular coding, loops, etc.
 
 def welcome():
-    print("Welcome to Guess the number!\n")
+    print("Welcome to Guess the number!\n")     
 
 def validate_input():
-    user_input = int(input("Enter a number between 1-100: "))  # user input
-    while True:
-        if 0 <= user_input <= 100:
-            break   #if condition is true, great. 
-        else:
-            user_input = int(input((f"{user_input} ins't valid. Enter a valid level within the range 1-100: ")))    # if not ask again
-    print(f"Input: {user_input} accepted!")
-    return user_input
+    while True:     # Loop until a valid number is chosen
+        try:
+            user_input = int(input("Enter a number between 1-100: "))
+            if 0 <= user_input <= 100:
+                print(f"Input: {user_input} accepted!")
+                return user_input       # When inputted number is within range, return that number
+            else:
+                print(f"{user_input} isn't valid. Enter a number between 1-100.")
+        except ValueError:      # Throw a value error when not an int
+            print("You did not input an int!")
+
 
 def generate_number():
     random_generated = (random.randint(1, 100))
-    return random_generated
+    return random_generated     # Get random number
 
-def game(user_num, rand_num):
+def game(user_num, rand_num):   # Main logic
     while True:
         if user_num > rand_num:
             print("Too high!")
